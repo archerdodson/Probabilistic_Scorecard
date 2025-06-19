810c464a6aabdc43b14cb3db3a1df19aea628867
archerdodson <archerdodson@gmail.com>
2025-06-19 18:46:01 +0100

    Update runcompute.sh

--- runcompute.sh
+++ runcompute.sh
@@ -18,11 +18,9 @@ module load PyTorch/2.1.2-CUDA-12.1.1
 
 #module load SciPy-bundle  
 
-source ~/newenv2/bin/activate  #Change environment
+source ~/newenv4/bin/activate  #Change environment
 
-#unset PYTHONPATH
-
-pip show zarr numcodecs xarray-beam rechunker pandas torch weatherbench2
+#pip show zarr numcodecs xarray-beam rechunker pandas torch weatherbench2
 
 srun python ProbScoreCard.py
 
