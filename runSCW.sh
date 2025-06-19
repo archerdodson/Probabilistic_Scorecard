c3f52c349d02e61bc9355b114a2d48e2d5c5ec30
archerdodson <archerdodson@gmail.com>
2025-06-20 00:33:01 +0100

    Update runSCW.sh

--- runSCW.sh
+++ runSCW.sh
@@ -17,7 +17,7 @@ module load OpenMPI/4.1.5
 module load PyTorch/2.1.2-CUDA-12.1.1
 #module load SciPy-bundle  
 
-source ~/envcard/bin/activate  #Change environment
+source ~/newenv4/bin/activate  #Change environment
 
 srun python ScoreCardWeather.py
 
